edf39389b88673a1c8350c48619e0229d147df9a
asselby <asselid@bk.ru>
2018-03-29 17:53:01 +0600

    inital

---
+++ XORHW/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace XORHW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.nameFileTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLable = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.encryptRadioButton = new System.Windows.Forms.RadioButton();
+            this.decipherRadioButton = new System.Windows.Forms.RadioButton();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.startButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.selectFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.selectFileButton.Location = new System.Drawing.Point(22, 26);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(98, 29);
+            this.selectFileButton.TabIndex = 0;
+            this.selectFileButton.Text = "Файл";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            // 
+            // nameFileTextBox
+            // 
+            this.nameFileTextBox.Location = new System.Drawing.Point(127, 26);
+            this.nameFileTextBox.Multiline = true;
+            this.nameFileTextBox.Name = "nameFileTextBox";
+            this.nameFileTextBox.Size = new System.Drawing.Size(363, 29);
+            this.nameFileTextBox.TabIndex = 1;
+            // 
+            // passwordLable
+            // 
+            this.passwordLable.AutoSize = true;
+            this.passwordLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwordLable.Location = new System.Drawing.Point(20, 103);
+            this.passwordLable.Name = "passwordLable";
+            this.passwordLable.Size = new System.Drawing.Size(80, 25);
+            this.passwordLable.TabIndex = 2;
+            this.passwordLable.Text = "Пароль";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(127, 100);
+            this.passwordTextBox.Multiline = true;
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(130, 31);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // encryptRadioButton
+            // 
+            this.encryptRadioButton.AutoSize = true;
+            this.encryptRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.encryptRadioButton.Location = new System.Drawing.Point(263, 100);
+            this.encryptRadioButton.Name = "encryptRadioButton";
+            this.encryptRadioButton.Size = new System.Drawing.Size(154, 28);
+            this.encryptRadioButton.TabIndex = 4;
+            this.encryptRadioButton.TabStop = true;
+            this.encryptRadioButton.Text = "Зашифровать";
+            this.encryptRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // decipherRadioButton
+            // 
+            this.decipherRadioButton.AutoSize = true;
+            this.decipherRadioButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.decipherRadioButton.Location = new System.Drawing.Point(422, 100);
+            this.decipherRadioButton.Name = "decipherRadioButton";
+            this.decipherRadioButton.Size = new System.Drawing.Size(163, 28);
+            this.decipherRadioButton.TabIndex = 5;
+            this.decipherRadioButton.TabStop = true;
+            this.decipherRadioButton.Text = "Расшифровать";
+            this.decipherRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(22, 166);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(369, 27);
+            this.progressBar.TabIndex = 6;
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startButton.Location = new System.Drawing.Point(415, 166);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 27);
+            this.startButton.TabIndex = 7;
+            this.startButton.Text = "Пуск";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancelButton.Location = new System.Drawing.Point(510, 166);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 27);
+            this.cancelButton.TabIndex = 8;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 230);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.decipherRadioButton);
+            this.Controls.Add(this.encryptRadioButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLable);
+            this.Controls.Add(this.nameFileTextBox);
+            this.Controls.Add(this.selectFileButton);
+            this.Name = "Form1";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.TextBox nameFileTextBox;
+        private System.Windows.Forms.Label passwordLable;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.RadioButton encryptRadioButton;
+        private System.Windows.Forms.RadioButton decipherRadioButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
+
---
+++ XORHW/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XORHW
+{
+    public partial class Form1 : Form
+    {
+        private byte password;
+        OpenFileDialog openFile = new OpenFileDialog();
+        public Form1()
+        {
+            InitializeComponent();
+            selectFileButton.Click += FileSelectButtonClick;
+            startButton.Click += StartButtonClick;
+        }
+
+        void FileSelectButtonClick(object sender, EventArgs e)
+        {
+            openFile.ShowDialog();
+            nameFileTextBox.Text = openFile.FileName;
+        }
+
+        void StartButtonClick(object sender, EventArgs e)
+        {
+            if (encryptRadioButton.Checked && passwordTextBox.Text!=String.Empty)
+            {
+                Byte.TryParse(passwordTextBox.Text, out password);
+                using (var encryptFile = File.Open(openFile.FileName, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    while (encryptFile.Position < encryptFile.Length)
+                    {
+                        progressBar.Minimum = 0;
+                        progressBar.Maximum = 10;
+                        byte[] array = new byte[encryptFile.Length];
+                        encryptFile.Read(array, 0, array.Length);
+                        for (int i = 0; i < array.Length; i++)
+                        {
+                            array[i] ^= password;
+                            encryptFile.Seek(0, SeekOrigin.Begin);
+                            encryptFile.Write(array, 0, array.Length);
+                            progressBar.Increment((int)encryptFile.Length);
+                        }
+                        MessageBox.Show("Файл зашифрован");
+                    }
+                }
+            }
+
+            if (decipherRadioButton.Checked)
+            {
+                byte secondPassword;
+                Byte.TryParse(passwordTextBox.Text, out secondPassword);
+                if (password == secondPassword)
+                {
+                    using (var decipherFile = File.Open(openFile.FileName, FileMode.Open, FileAccess.ReadWrite))
+                    {
+                        while (decipherFile.Position < decipherFile.Length)
+                        {
+                            progressBar.Minimum = 0;
+                            progressBar.Maximum = 10;
+                            byte[] array = new byte[decipherFile.Length];
+                            decipherFile.Read(array, 0, array.Length);
+                            for (int i = 0; i < array.Length; i++)
+                            {
+                                array[i] ^= password;
+                                decipherFile.Seek(0, SeekOrigin.Begin);
+                                decipherFile.Write(array, 0, array.Length);
+                                progressBar.Increment((int) decipherFile.Length);
+                            }
+
+                            MessageBox.Show("Файл расшифрован");
+                        }
+                    }
+                }
+                else MessageBox.Show("Пароли не совпадают");
+            }
+            else if (passwordTextBox.Text==String.Empty)
+                MessageBox.Show("Пароль не введен либо не выбрана функция!");
+        }
+    }
+}
